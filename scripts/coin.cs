b22ce20e0cebeec0dad28072def6e1f862ee689a
Ellbow <ellbowdev@protonmail.com>
2024-05-30 01:31:57 +0100

    Testing with signals with onbodyentered on coin

--- scripts/coin.cs
+++ scripts/coin.cs
@@ -3,15 +3,27 @@ using System;
 
 public partial class coin : Area2D
 {
+	int count = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
+		BodyEntered += OnBodyEntered;
+
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+		
 	}
 
-	
+	private void OnBodyEntered(Node body)
+	{
+		count++;
+		GD.Print("body entereed " + count);
+	}
 }
