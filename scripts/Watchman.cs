aa57f77b98550e488c22475764d9aa6bb443caf8
Ellbow <ellbowdev@protonmail.com>
2024-08-05 20:57:09 +0100

    Finish up tutorial on state chart. Add a new parallel state to better manage multiple states normal/berserk/idle etc. Add a count to pass expression in new Berserk state. Update the sprite to reflect current state

--- scripts/Watchman.cs
+++ scripts/Watchman.cs
@@ -4,7 +4,8 @@ using System;
 
 public partial class Watchman : Node2D
 {
-    Enemy enemy = null;
+    private Enemy enemy = null;
+    public int enemyEnteredCount = 0;
 
     private StateChart stateChart;
 
@@ -13,12 +14,20 @@ public partial class Watchman : Node2D
 
     public override void _Ready()
     {
+        // ⚠️ Note: The initial state of a state chart will only be entered one frame after the state chart's _ready function ran. 
+        // It is done this way to give nodes above the state chart time to run their _ready functions before any state chart logic is triggered.
+        // This means that if you call send_event, set_expression_property or step in a _ready function things will most likely not work as expected. 
+        // If you must call any of these functions in a _ready function, you can use call_deferred to delay the event sending by one frame, e.g. state_chart.send_event.call_deferred("some_event").
+        
         area.AreaEntered += OnAreaEntered2D;
         area.AreaExited += OnAreaExited2D;
         stateChart = StateChart.Of(GetNode("StateChart"));
 
-        // get the active state node
-        var stateEnt = StateChartState.Of(GetNode("%Idle"));
+        PrintTree(GetTree().Root);
+
+        var idlenode = GetNode("StateChart/Root/AlertState/Idle");
+
+        var stateEnt = StateChartState.Of(idlenode);
         // connect to the state_entered signal
         stateEnt.Connect(StateChartState.SignalName.StateEntered, Callable.From(OnStateEntered));
 
@@ -28,15 +37,17 @@ public partial class Watchman : Node2D
         // // connect to the state_entered signal
         // state.Connect(StateChartState.SignalName.StateProcessing, Callable.From(OnObservingStateProcessing));
                 
-        var state = StateChartState.Of(GetNode("%Observing"));
+        var state = StateChartState.Of(GetNode("StateChart/Root/AlertState/Observing"));
         //state.Connect(StateChartState.SignalName.StateProcessing, new Callable(this, nameof(OnObservingStateProcessing)));
         state.Connect(StateChartState.SignalName.StateProcessing, Callable.From((float delta) => OnObservingStateProcessing(delta)));
     }
-    
 
     private void OnAreaEntered2D(Area2D area)
     {
+        enemyEnteredCount++;
+        stateChart.SetExpressionProperty("enemyEnteredCount", enemyEnteredCount);
         stateChart.SendEvent("EnemyEntered");
+        
         enemy = area.GetParent() as Enemy;
     }
 
@@ -54,5 +65,26 @@ public partial class Watchman : Node2D
     {
         RotationDegrees = -90;
         enemy = null;
+    }    
+    
+    private void OnNormalStateEntered()
+    {
+        GetNode<Sprite2D>("Sprite2D").Modulate = new Color(1,1,1);
+        enemyEnteredCount = 0;
+        stateChart.SetExpressionProperty("enemyEnteredCount", enemyEnteredCount);
+    }
+
+    private void OnBerserkStateEntered()
+    {
+        GetNode<Sprite2D>("Sprite2D").Modulate = new Color(1, 0, 0);
+    }
+
+    private void PrintTree(Node node, int indent = 0)
+    {
+        GD.Print(new string(' ', indent * 2) + node.Name);
+        foreach (Node child in node.GetChildren())
+        {
+            PrintTree(child, indent + 1);
+        }
     }
 }
